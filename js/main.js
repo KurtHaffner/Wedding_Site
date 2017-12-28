1ec1f6ee061f56eb3fe89a7038e44b0d878bf687
Unknown <kurly519@gmail.com>
2017-12-27 22:40:52 -0500

    Updated Countdown
    Updated the time on the countdown to be 17:00:00.

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Oct 20, 2018 15:37:25").getTime();
+		var countDownDate = new Date("Oct 20, 2018 17:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
